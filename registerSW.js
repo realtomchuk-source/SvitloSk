eecb8aa6c4a2967ede868dc07a784da5a328775c
realtomchuk-source <real.tomchuk@gmail.com>
2026-04-23 13:50:08 +0000

    Deploying to gh-pages from @ realtomchuk-source/SvitloSk@b2d7e09be648c06c7590dda9898d48f49ff25081 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/SvitloSk/sw.js', { scope: '/SvitloSk/' })})}
